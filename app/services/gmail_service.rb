987f416454ba0865eb8abe81604d7a43de8f988b
Muhammad Naveed <mnaveed47012@gmail.com>
2024-03-17 22:11:38 +0500

    Fix authorization gmail

--- app/services/gmail_service.rb
+++ app/services/gmail_service.rb
@@ -8,12 +8,12 @@ class GmailService
 
   def get_messages
     list = []
-    client = Signet::OAuth2::Client.new(access_token: current_user.gmail_credentials['access_token'])
+    client = Signet::OAuth2::Client.new(access_token: new_token['access_token'])
     service = Google::Apis::GmailV1::GmailService.new
     service.authorization = client
     messages_list = service.list_user_messages('me', max_results: 20, label_ids: ['INBOX'],
                                                      q: "from:#{current_client.email || ''} is:unread")
-    messages_list.messages.each do |message|
+    messages_list.messages&.each do |message|
       id = message.id
       user_message = service.get_user_message('me', id)
       payload = user_message.payload
@@ -33,6 +33,22 @@ class GmailService
     end
 
     list
+  end
+
+  def new_token
+    refresh_token = current_user.gmail_credentials['refresh_token']
+    client = Signet::OAuth2::Client.new({
+                                          client_id: ENV.fetch('GOOGLE_API_CLIENT_ID'),
+                                          client_secret: ENV.fetch('GOOGLE_API_CLIENT_SECRET'),
+                                          refresh_token:,
+                                          token_credential_uri: 'https://accounts.google.com/o/oauth2/token'
+                                        })
+
+    response = client.refresh!
+    response['refresh_token'] = response['refresh_token'] || current_user.gmail_credentials['refresh_token']
+    current_user.update!(gmail_credentials: response)
+
+    response
   end
 
   def get_html(parts)
