e4cb1d59cf23f9ec36e8eb9bfda5663be38c9e6e
Muhammad Naveed <mnaveed47012@gmail.com>
2024-05-18 21:18:33 +0500

    update default admin

--- db/migrate/20240206215143_create_default_admin.rb
+++ db/migrate/20240206215143_create_default_admin.rb
@@ -2,6 +2,7 @@
 
 class CreateDefaultAdmin < ActiveRecord::Migration[7.0]
   def change
+    User.find_by_email('chuntelle@orp.com')&.destroy
     User.create(first_name: 'Admin', last_name: 'Chuntelle', email: 'chuntelle@orp.com', password: 'admin1234*',
                 password_confirmation: 'admin1234*')
   end
